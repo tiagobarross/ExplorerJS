50aaf6ca2fb792037a3924c9d70680a1cefdb392
Tiago <tiagobarros519@gmail.com>
2024-09-12 10:38:02 -0300

    final

---
+++ main.js
@@ -0,0 +1,86 @@
+/*//1 
+alert("Hello World");
+
+//2
+let num1 = 1
+let num2 = 2
+
+alert(`A soma das variáveis é: ${num1 + num2}`)
+
+//3
+let num = prompt("Insira um dado:")
+
+if(isNaN(num)){
+  alert("O dado inserido não é um número.")
+}
+else{
+  alert("o dado inserido é um número.")
+}
+
+//4
+let string = prompt("Digite um dado:")
+
+if(isNaN(string)){
+  alert("O dado inserido é uma string")
+}
+else{
+  alert("O dado inserido não é uma string")
+}
+
+//5
+let boolean = prompt("Digite um dado:")
+
+if(boolean.toLowerCase() === "true" | boolean.toLowerCase() === "false"){
+  alert("O dado inserido é um boolean")
+}
+else{
+  alert("O dado inserido não é um boolean")
+}
+
+//6
+let num1 = Number(prompt("Digite o primeiro número:"))
+let num2 = Number(prompt("Digite o segundo número:"))
+
+alert(`O resultado da subtraçãio dos números é:${num1 - num2}`)
+
+//7
+let num1 = Number(prompt("Digite o primeiro número:"))
+let num2 = Number(prompt("Digite o segundo número:"))
+
+alert(`O resultado da múltiplicação dos números é:${num1 * num2}`)
+
+//8
+let num1 = Number(prompt("Digite o primeiro número:"))
+let num2 = Number(prompt("Digite o segundo número:"))
+
+alert(`O resultado da divisão dos números é:${num1 / num2}`)
+
+//9
+let num = Number(prompt("Digite um número:"))
+
+if(!isNaN(num)){
+  if((num % 2) === 0){
+    alert("O número inserido é par.")
+  }
+  else{
+    alert("O número inserido é ímpar.")
+  }
+}
+else{
+  alert("O dado inserido não é um número válido")
+}
+
+//10
+let num = Number(prompt("Digite um número:"))
+
+if(!isNaN(num)){
+  if((num % 2) !== 0){
+    alert("O número inserido é ímpar.")
+  }
+  else{
+    alert("O número inserido não é ímpar.")
+  }
+}
+else{
+  alert("O dado inserido não é um número válido")
+}*/
